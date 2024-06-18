0665d708eee506c62b9f34a73265b572cd82f20a
Krishnan <sunilkrish2004@gmail.com>
2024-06-18 09:50:19 +0530

    responsive

--- src/Items.js
+++ src/Items.js
@@ -7,14 +7,14 @@ const Items = ({items,setItems,addItems,setAddItems}) => {
      
     return (
             <main className="m-8 border rounded-2xl border-gray-400">
-            <div className="m-8 border rounded-2xl border-gray-400">
+            <div className="m-2 border rounded-2xl border-gray-400">
                         <ItemHeader/>
                         <ListItems items={items} setItems={setItems} />
                         {items.length === 0 &&(
                         <p className="text-center">Empty</p>
                         )}
                         </div>
-    <div className="m-8 border rounded-2xl border-gray-400">
+    <div className="m-2 border rounded-2xl border-gray-400">
                 <ItemContent items={items} setItems={setItems} addItems={addItems} setAddItems={setAddItems} />
                 </div>
             </main>
